0c7263193797e00c8daf1f6243b2733314b41053
regis <regis@rtmtag.com>
2020-03-06 21:50:02 -0300

    Father and son

---
+++ father_and_son.py
@@ -0,0 +1,8 @@
+'''
+A script that match the name of two people
+Compare last name of two people and see if they are of the same family
+'''
+if "Will Smith".split()[1] == "Jack Smith".split()[1]:
+    print("Same")
+else:
+    print("Different")
